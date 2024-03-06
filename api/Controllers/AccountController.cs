a0876dc22b0308b690b8a523cbe4ddd288cb7d4e
Apolos666 <quangprox121@gmail.com>
2024-03-06 15:25:01 +0700

    Added Login Endpoint to login account and retrieve Token

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
@@ -11,12 +12,47 @@ namespace api.Controllers;
 public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
     private readonly ITokenService _tokenService;
 
-    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
         _tokenService = tokenService;
+    }
+    
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDTO.UserName);
+            
+            if (user is null)
+                return Unauthorized("Invalid username!");
+            
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+            
+            if (result.Succeeded)
+                return Ok(
+                    new NewUserDTO
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
+            else
+                return Unauthorized("Invalid username or password!");
+        } catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
     }
     
     [HttpPost]
---
+++ api/DTOs/AccountDTO/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.DTOs.AccountDTO;
+
+public class LoginDTO
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
