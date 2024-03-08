ad8697ee8b8ebee4dfa1a1024e40926d976c2073
Apolos666 <quangprox121@gmail.com>
2024-03-09 00:21:23 +0700

    Create a login page and perform validation using the yup library, and try to login -> success

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -9,6 +9,7 @@ import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import DesignPage from "../Pages/DesignPage/DesignPage";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import CashFlowStatement from "../Components/CashFlowStatement/CashFlowStatement";
+import {LoginPage} from "../Pages/LoginPage/LoginPage";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,7 @@ export const router = createBrowserRouter([
         element: <App />,
         children: [
             { path: '', element: <HomePage /> },
+            { path: 'login', element: <LoginPage /> },
             { path: 'search', element: <SearchPage /> },
             { path: 'design-guide', element: <DesignPage /> },
             { path: 'company/:ticker', element: <CompanyPage />,
